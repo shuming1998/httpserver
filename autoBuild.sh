9e6bbc2957aa78848af4bdacb4fec4ada2eb0695
StudyingCui <1005586890@qq.com>
2022-12-28 13:21:35 +0800

    Update autoBuild.sh

--- autoBuild.sh
+++ autoBuild.sh
@@ -11,16 +11,3 @@ rm -rf `pwd`/build/*
 cd `pwd`/build &&
     cmake .. &&
     make -j16
-
-# cd ..
-
-# if [ ! -d /usr/include/mymuduo ]; then
-#     mkdir /usr/include/mymuduo
-# fi
-
-# for header in `ls *.h`
-# do
-#     cp $header /usr/include/mymuduo
-# done
-
-# cp `pwd`/lib/libmyMuduo.so /usr/lib
